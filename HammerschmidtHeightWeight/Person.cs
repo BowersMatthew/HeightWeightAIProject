935368a535de6c36d4cd88212d95cb778c87f797
BowersMatthew <bowersma@mymail.vcu.edu>
2017-09-30 14:02:42 -0400

    fixed Person and added PersonTest.cs to make sure it more or less works

--- HammerschmidtHeightWeight/Person.cs
+++ HammerschmidtHeightWeight/Person.cs
@@ -1,57 +1,54 @@
-namespace Person
+using System;
+
+public class Person 
 {
-    
-    public class Person 
+    static readonly int MALE = 0;
+    static readonly int FEMALE = 1;
+    // in cm
+    // these constants are derived from the WHO standard growth charts
+    static readonly double maleAveHeight = 176.5;
+    static readonly double maleSdHeight = 6.2;
+    static readonly double femaleAveHeight = 163.3;
+    static readonly double femaleSdHeight = 5.5; 
+    // Block, J. P., Subramanian, S. V., Christakis, N. A., & O�Malley, 
+    // A. J. (2013). Population Trends and Variation in Body Mass Index 
+    // from 1971 to 2008 in the Framingham Heart Study Offspring Cohort. 
+    // PLoS ONE, 8(5), e63217. http://doi.org/10.1371/journal.pone.0063217
+    // kg/m/m
+    static readonly double maleAveBMI = 29;
+    static readonly double maleSdBMI = 4.73;
+    static readonly double femaleAveBMI = 27.7;
+    static readonly double femaleSdBMI = 6.15;
+
+    public double height;
+    public double weight;
+    public int sex;
+
+    // Constructor throws ArgumentException if sex is not 0 or 1
+    public Person(double zH, double zW, int sex)
     {
-
-        const int MALE = 0;
-        const int FEMALE = 1;
-        // in cm
-        // these constants are derived from the WHO standard growth charts
-        const double maleAveHeight = 176.5;
-        const double maleSdHeight = 6.2;
-        const double femaleAveHeight = 163.3;
-        const double femaleSdHeight = 5.5; 
-        // Block, J. P., Subramanian, S. V., Christakis, N. A., & O�Malley, 
-        // A. J. (2013). Population Trends and Variation in Body Mass Index 
-        // from 1971 to 2008 in the Framingham Heart Study Offspring Cohort. 
-        // PLoS ONE, 8(5), e63217. http://doi.org/10.1371/journal.pone.0063217
-        // kg/m/m
-        const double maleAveBMI = 29;
-        const double maleSdBMI = 4.73;
-        const double femaleAveBMI = 27.7;
-        const double femaleSdBMI = 6.15;
-
-        public double height;
-        public double weight;
-        public int sex;
-
-        // Constructor throws ArgumentException if sex is not 0 or 1
-        public Person(double zH, double zW, int sex)
+        this.sex = sex;
+        if(sex == MALE)
         {
-            this.sex = sex;
-            if(sex == MALE)
-            {
-                setHeight(maleAveHeight, maleSdHeight, zH);
-                setWeight(maleAveBMI, maleSdBMI, zW);
-            }else if(sex == FEMALE)
-            {
-                setHeight(femaleAveHeight, femaleSdHeight, zH);
-                setWeight(femaleAveBMI, femaleSdBMI, zW);
-            }else
-            {
-                throw new ArgumentException("unknown sex.")
-            }
-        }
-
-        private void setHeight(double aveHeight, double sdHeight, double hz) 
+            setHeight(maleAveHeight, maleSdHeight, zH);
+            setWeight(maleAveBMI, maleSdBMI, zW);
+        }else if(sex == FEMALE)
         {
-            height = hz * sdHeight + aveHeight;
-        }
-        private void setWeight(double aveBMI, double sdBMI, double BMIz) 
+            setHeight(femaleAveHeight, femaleSdHeight, zH);
+            setWeight(femaleAveBMI, femaleSdBMI, zW);
+        }else
         {
-            weight = (BMIz * sdBMI + aveBMI) * height / 100 * height / 100;
+            throw new ArgumentException("unknown sex.");
         }
-
     }
+
+    private void setHeight(double aveHeight, double sdHeight, double hz) 
+    {
+        height = hz * sdHeight + aveHeight;
+    }
+    private void setWeight(double aveBMI, double sdBMI, double BMIz) 
+    {
+        weight = (BMIz * sdBMI + aveBMI) * height / 100 * height / 100;
+    }
+
 }
