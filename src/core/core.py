d054458cfb527569b00d5725e671e39b599c34f3
BowersMatthew <bowersma@mymail.vcu.edu>
2017-10-02 21:49:34 -0400

    adding updates to core.py

--- src/core/core.py
+++ src/core/core.py
@@ -1,15 +1,25 @@
 from src.Data.Person import Person
-import numpy.random
+from src.Data.normalize import normalize
 
-people = []
+people = normalize(Person.makepeople(400))
 
-for i in range(0, 40):
-    if i < 20:
-        s = 0
-    else:
-        s = 1
-    a = Person(numpy.random.normal(0, 1), numpy.random.normal(0, 1), s)
-    people.append(a)
+
+w = [1, 1, -1]
+k = .02
+
+print(str(people[2].height))
+
+for i in range(0, 400):
+    for p in people:
+        if p.height * w[0] + p.weight * w[1] + w[2] < 0:
+            net = -1
+        else:
+            net = 1
+        w[0] += k * p.height * (p.sex - net)
+        w[1] += k * p.weight * (p.sex - net)
+        w[2] += k * (net - p.sex)
+    print(str(w))
+
 
 f = open('data.txt', 'w')
 for p in people:
