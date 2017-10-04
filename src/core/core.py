9c0466f7216b599f51427fa0c7092603d8f3a92a
kshammer <hammerschmiks@vcu.edu>
2017-10-04 01:43:56 -0400

    Testing CoolGraph

--- src/core/core.py
+++ src/core/core.py
@@ -7,7 +7,7 @@ from src.training.train import Train
 people = minmaxnorm(Person.makepeople(4000))
 params = [[1, 1, 1], 0.01, 400, 0.00001]
 hard = Train(people, params)
-
+"""
 for e in hard.errors:
     print(str(e))
 print("Best weight: " + str(hard.bestw) + "Best err:" + str(hard.besterr))
@@ -30,6 +30,7 @@ for p in people:
     f.write(str(p.height) + "," + str(p.weight) + "," + str(p.sex) + "," + str(p.pred) + "\n")
 
 f.close()
-coolgraph(people)
+"""
+coolgraph(people, hard.bestw)
 
 
