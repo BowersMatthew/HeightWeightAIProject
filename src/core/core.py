bf3f34b7402e04519452be8c5939005f8dbaeba8
BowersMatthew <bowersma@mymail.vcu.edu>
2017-10-05 15:06:22 -0400

    not sure what I changed but it is mad

--- src/core/core.py
+++ src/core/core.py
@@ -6,9 +6,10 @@ from src.training.train import Train
 
 people = minmaxnorm(Person.makepeople(4000))
 params = [[1, 1, 1], 0.01, 1000, 0.00001]
-hard = Train(people, params)
-coolgraph(hard.data, hard.bestw)
+#hard = Train(people, params)
+#coolgraph(hard.data, hard.bestw)
 
-params = [[1, 1, 1], .01, 1, 1000, 0.00001]
+# params [ [initial weights], alpha, gain, num iterations, desired error]
+params = [[1, 1, 1], .2, 0.1, 1000, 0.00001]
 soft = Train(people, params)
 coolgraph(soft.data, soft.bestw)
