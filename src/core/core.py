b7093deebdb5ece857ff3b6958e840194ebbe193
kshammer <hammerschmiks@vcu.edu>
2017-10-08 14:38:20 -0400

    Added error graph

--- src/core/core.py
+++ src/core/core.py
@@ -4,6 +4,7 @@ from src.graph.coolgraph import coolgraph
 from src.Data.normalize import minmaxnorm
 from src.training.train import Train
 from multiprocessing import Process
+from src.graph.errorgraph import errorgraph
 import os
 
 if __name__ == '__main__':
@@ -30,6 +31,7 @@ if __name__ == '__main__':
         # hard params order should be [starting weights, alpha, max iterations, desired error, cut, num people]
         params = [[1, 1, 1], 0.05, 1000, 0.00001, cut, numpeople]
         hard = Train(people, params)
+        errorgraph(hard)
         hardsets.append(hard)
         # write this result to file
         tofile(os.path.join(os.path.realpath(''),
@@ -42,6 +44,7 @@ if __name__ == '__main__':
         # soft params order should be [starting weights, alpha, gain, max iterations, desired error, cut, num people]
         params = [[1, 1, 1], 0.1, 0.005, 1000, 0.00001, cut, numpeople]
         soft = Train(people, params)
+        errorgraph(soft)
         softsets.append(soft)
         # write this result to file
         tofile(os.path.join(os.path.realpath(''),
--- src/graph/errorgraph.py
+++ src/graph/errorgraph.py
@@ -1,1 +1,9 @@
 import matplotlib.pyplot as plt
+from src.Data.Person import Person
+
+def errorgraph(data):
+    error = data.errors
+    x = list(range(0, len(data.errors)))
+    plt.scatter(x, error, alpha=0.8, edgecolors='none', s=10)
+    plt.axis([0, 1000, 0, max(error) + max(error)/10])
+    plt.show()
