264d3217fba8f2a1597192f9e888cf995de9d652
kshammer <hammerschmiks@vcu.edu>
2017-10-08 15:59:16 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HeightWeightAIProject',
+    version='1',
+    packages=['src.core', 'src.Data', 'src.graph', 'src.training'],
+    url='',
+    license='',
+    author='Kyle Hammerschmidt, Matt Bowers',
+    author_email='hammerschmiks@vcu.edu, bowersma@vcu.edu',
+    description='409 Project 2'
+)
