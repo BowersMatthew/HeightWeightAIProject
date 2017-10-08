f581838f76eccaa91a21210a6f96c160aea89b73
BowersMatthew <bowersma@mymail.vcu.edu>
2017-10-08 00:14:05 -0400

    put main on a loop to go through each division of data we need. Perhaps spin 1 thread for each?

--- src/core/core.py
+++ src/core/core.py
@@ -9,9 +9,8 @@ import os
 if __name__ == '__main__':
     # number of people in the whole data set
     numpeople = 4000
-
-    # percent to use for training
-    cut = 50
+    hardsets = []
+    softsets = []
 
     # generate the people
     people = Person.makepeople(numpeople)
@@ -25,33 +24,37 @@ if __name__ == '__main__':
     # normalize the people
     people = minmaxnorm(people)
 
-    # hard params order should be [starting weights, alpha, max iterations, desired error, cut, num people]
-    params = [[1, 1, 1], 0.05, 1000, 0.00001, cut, numpeople]
-    hard = Train(people, params)
+    for cut in (75, 50, 25):
+        # percent to use for training
+        # cut = 50
+        # hard params order should be [starting weights, alpha, max iterations, desired error, cut, num people]
+        params = [[1, 1, 1], 0.05, 1000, 0.00001, cut, numpeople]
+        hard = Train(people, params)
+        hardsets.append(hard)
+        # write this result to file
+        tofile(os.path.join(os.path.realpath(''),
+                            ('..\\..\\data\\hard' + str(cut) + str(100 - cut) + ".txt")), str(hard.bestw))
 
-    # write this result to file
-    tofile(os.path.join(os.path.realpath(''),
-                        ('..\\..\\data\\hard' + str(cut) + str(100 - cut) + ".txt")), str(hard.bestw))
+        # spin a thread to graph the result
+        p = Process(target=coolgraph, args=(hard.test, hard.bestw, 'hard'))
+        p.start()
 
-    # spin a thread to graph the result
-    p = Process(target=coolgraph, args=(hard.test, hard.bestw, 'hard'))
-    p.start()
+        # soft params order should be [starting weights, alpha, gain, max iterations, desired error, cut, num people]
+        params = [[1, 1, 1], 0.1, 0.005, 1000, 0.00001, cut, numpeople]
+        soft = Train(people, params)
+        softsets.append(soft)
+        # write this result to file
+        tofile(os.path.join(os.path.realpath(''),
+                            ('..\\..\\data\\soft' + str(cut) + str(100 - cut) + ".txt")), str(soft.bestw))
 
-    # soft params order should be [starting weights, alpha, gain, max iterations, desired error, cut, num people]
-    params = [[1, 1, 1], 0.1, 0.005, 1000, 0.00001, cut, numpeople]
-    soft = Train(people, params)
+        # spin a thread to graph the result
+        p1 = Process(target=coolgraph, args=(soft.test, soft.bestw, 'soft'))
+        p1.start()
+        p.join()
+        p1.join()
 
-    # write this result to file
-    tofile(os.path.join(os.path.realpath(''),
-                        ('..\\..\\data\\soft' + str(cut) + str(100 - cut) + ".txt")), str(soft.bestw))
-
-    # spin a thread to graph the result
-    p1 = Process(target=coolgraph, args=(soft.test, soft.bestw, 'soft'))
-    p1.start()
-    p.join()
-    p1.join()
-
-    # generate confusion matrices
-    hard.calcconfusion()
-    soft.calcconfusion()
+    for h in hardsets:
+        h.calcconfusion()
+    for s in softsets:
+        s.calcconfusion()
 
