8b03cc013cf80bb69972c4af301737bb5d9d6d7c
kshammer <hammerschmiks@vcu.edu>
2017-10-08 22:07:38 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HeightWeightAIProject',
+    version='0.1',
+    packages=['src.core', 'src.Data', 'src.graph', 'src.training'],
+    url='',
+    license='',
+    author='Bowers, Hammerschmidt',
+    author_email='',
+    description=''
+)
